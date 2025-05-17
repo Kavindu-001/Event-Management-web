e155842f3b63dc762c64a1760409f1d4de1a2831
Kavindu Janojya <kjanojya@gmail.com>
2025-05-18 00:36:08 +0530

    Update

---
+++ src/pages/AdminPanel/AdminMail.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import AdminSidebar from '../../components/AdminSidebar'
+
+const AdminMail = () => {
+  return (
+    <div>
+        <div>
+            <AdminSidebar />
+        </div>
+
+
+        <h1>AdminMail</h1>
+      
+    </div>
+  )
+}
+
+export default AdminMail
